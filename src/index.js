dbd0eb80e9cbd00858054d544875fb04056de935
Sébastien <contact@soulstudioart.com>
2025-09-25 16:28:32 -0400

    feat: Amélioration majeure de l'apparence du quiz (Soul Studio Art)changement index

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Nous allons injecter le script de Tailwind directement ici pour garantir son chargement.
+const TailwindScript = () => (
+    <script src="https://cdn.tailwindcss.com"></script>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <TailwindScript />
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
